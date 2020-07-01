964cf96f11d42559de42d3d7002b94a7018e9ef9
WJiaXin <1564187204@qq.com>
2020-07-01 17:42:04 +0800

    wjx1

--- src/main/java/com/hwadee/ssm/mappers/WorkOvertimeDao.java
+++ src/main/java/com/hwadee/ssm/mappers/WorkOvertimeDao.java
@@ -1,8 +1,10 @@
 package com.hwadee.ssm.mappers;
 
+import com.hwadee.ssm.entity.Logs;
 import com.hwadee.ssm.entity.WorkOvertime;
 import org.apache.ibatis.annotations.Mapper;
 import net.sf.json.JSONObject;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,6 @@ public interface WorkOvertimeDao {
     int postData(WorkOvertime workOvertime);
     int putData(WorkOvertime workOvertime);
     List<JSONObject> getDataUsers(String time);
+    List<JSONObject> getDataLogs(String time);
+    int putLogs(@Param("logId") Integer logId,@Param("state") Integer state);
 }
